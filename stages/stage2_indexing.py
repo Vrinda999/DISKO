5ab2941c5afa937cb59007aeb03e34cb32d3eceb
vrinda <abrolvrinda@gmail.com>
2025-02-09 18:13:26 +0530

    feat: error handling, closing file.

--- stages/stage2_indexing.py
+++ stages/stage2_indexing.py
@@ -1,6 +1,8 @@
+from os import close
 from elasticsearch import Elasticsearch
 import hashlib
 import os
+from tqdm import tqdm
 
 def index_metadata(image_path):
     if not os.path.exists(image_path):
@@ -12,8 +14,21 @@ def index_metadata(image_path):
     es = Elasticsearch("http://localhost:9200")
 
     # Calculate hash
-    with open(image_path, "rb") as f:
-        file_hash = hashlib.sha256(f.read()).hexdigest()
+    # with open(image_path, "rb") as f:
+    #     file_hash = hashlib.sha256(f.read()).hexdigest()
+
+    file_size = os.path.getsize(image_path)
+    file_hash = hashlib.sha256()
+    with open(image_path, "rb") as f, tqdm(
+        total = file_size, unit = "B", unit_scale = True, desc = "Hashing"
+    ) as progress:
+        while chunk := f.read(15360):      # read in 15MB chunks
+            file_hash.update(chunk)
+            progress.update(len(chunk))  # Update progress bar
+
+        file_hash = file_hash.hexdigest()
+    f.close()
+
 
     # Prepare metadata
     metadata = {
@@ -22,6 +37,13 @@ def index_metadata(image_path):
         "file_hash": file_hash,
     }
 
+
     # Index metadata
-    es.index(index="disk-images", document=metadata)
-    print(f"Metadata for {image_path} indexed successfully!")
+    try:
+        es.index(index="disk-images", document=metadata)
+        print(f"Metadata for {image_path} indexed successfully!")
+    except Exception as e:
+        print(f"Error indexing metadata: {e}")
+
+img = input("Enter Image Path (../op.dd): ")
+index_metadata(img)
