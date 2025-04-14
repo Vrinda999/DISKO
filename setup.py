715777d18d6317841465d8a6199d789b1d6d6f43
simmithapad <simmithapad23@gmail.com>
2025-04-14 10:36:18 -0400

    requirement and setup.py updated

--- setup.py
+++ setup.py
@@ -1,24 +1,42 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="disko",
+#     version="0.1.0",
+#     packages=find_packages(),
+#     include_package_data=True,
+#     install_requires=open("requirements.txt").read().splitlines(),
+#     entry_points={
+#         "console_scripts": [
+#             "disko = main:main"
+#         ],
+#     },
+#     author="simmithapad",
+#     description="DISKO: Disk Forensics Tool for Data Categorization & Keyword Filtering",
+#     long_description=open("README.md").read(),
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/simmithapad/DISKO",
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#     ],
+#     python_requires='>=3.8',
+# )
+
 from setuptools import setup, find_packages
 
 setup(
     name="disko",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
+    version="1.0.0",
+    packages=find_packages(include=["stages", "utils"]),  # include your folders as packages
+    py_modules=["main"],  # since main.py is a standalone module
     install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
         "console_scripts": [
-            "disko = main:main"
+            "disko = main:main",  # refers to main() in main.py
         ],
     },
-    author="simmithapad",
-    description="DISKO: Disk Forensics Tool for Data Categorization & Keyword Filtering",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/simmithapad/DISKO",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires='>=3.8',
+    author="Your Name",
+    description="DISKO - Disk Operation Tool for Data Categorization and Keyword Filtering",
+    license="MIT",
 )
