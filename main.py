8a9def13cf06e2c82664ba3bc7af7ae0d77adc0b
vrinda <abrolvrinda@gmail.com>
2025-04-18 20:18:22 +0530

    feat: keyword filtering from text files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from stages.stage1_disk_imaging import run_dcfldd
 from stages.stage2_extraction import analyze_disk_image
 from stages.stage3_categorization import categorize_data
 from stages.stage4_filtering import get_files_by_type
-from stages.stage4_2_keyword import mount_and_extract_text_files
+from stages.stage4_2_keyword import MasterFunc
 from stages.stage5_reporting import generate_report
 
 def run_command(command):
@@ -37,11 +37,11 @@ def main():
         return
 
     # Stage 3: Data Categorization
-    print("\n📂 Proceeding to data categorization...")
-    categorized_output = categorize_data(disk_image, start_sector)
-    if not categorized_output:
-        print("❌ Categorization failed. Exiting...")
-        return
+    # print("\n📂 Proceeding to data categorization...")
+    # categorized_output = categorize_data(disk_image, start_sector)
+    # if not categorized_output:
+    #     print("❌ Categorization failed. Exiting...")
+    #     return
 
     # Stage 4
     print("\n📂 Proceeding to Filtering...")
@@ -60,30 +60,23 @@ def main():
                 print()
         else:
             print("No files found with the specified extensions.")
-    
-    # Keyword Filtering
-    # keywords = input("Enter Keywords (comma-separated, e.g.: Lorem, Ipsum, dolor): ").split(",")
-    # if keywords[0] == '':
-    #     print("No Keywords Chosen")
-    # else:
-    #     output_dir = input("Enter Output Directory (e.g.: ./output_files/extracted_files): ")
-    #     Master_Func(disk_image, keywords, output_dir, start_sector)
-    
+        
 
     # Keyword New Trial
-    image_path='TEST.E01'
-    output_dir='./output_files/extracted_files'
-
-    image_name = os.path.basename(image_path)
+    image_name = os.path.basename(disk_image)
     image_stem = os.path.splitext(image_name)[0]
-    output_dir = os.path.join(output_dir, image_stem)
 
-    mount_and_extract_text_files(image_path, output_dir, start_sector)
-
+    keywords = input("Enter Keywords (comma-separated, e.g.: Lorem, Ipsum, dolor): ").split(",")
+    if keywords[0] == '':
+        print("No Keywords Chosen")
+    else:
+        output_dir = input("Enter Output Directory (e.g.: ./output_files/extracted_files): ")
+        output_dir = os.path.join(output_dir, image_stem)
+        MasterFunc(disk_image, keywords, output_dir, start_sector)
 
     # Stage 5: Report Generation
     print("\n📝 Generating PDF report...")
-    generate_report(disk_image, categorized_output)
+    # generate_report(disk_image, categorized_output)
 
 if __name__ == "__main__":
     main()
--- stages/stage4_2_keyword.py
+++ stages/stage4_2_keyword.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 from stages.stage2_extraction import run_command
 
@@ -22,8 +23,6 @@ def mount_and_extract_text_files(image_path, output_dir, start_sector):
             image_path = os.path.join(ewf_mount_point, 'ewf1')
         
 
-        print(f"{"-"*100}\nMMLS START SECTOR!!! = {start_sector}\n\n")
-
         # Mount the partition using offset
         offset = int(start_sector) * 512
         run_command(f"sudo mount -o loop,ro,offset={offset} {image_path} {mount_dir}")
@@ -36,8 +35,8 @@ def mount_and_extract_text_files(image_path, output_dir, start_sector):
                     relative_path = os.path.relpath(source_path, mount_dir)
                     dest_path = os.path.join(output_dir, relative_path)
                     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-                    print(f'Copying Files Now. Root: {root}, \nDirs: {dirs}, \nfile: {file}\n\n')
-                    print(f"src path: {source_path}, \nrel path: {relative_path}, \nDest Path: {dest_path}\n\n")
+                    # print(f'Copying Files Now. Root: {root}, \nDirs: {dirs}, \nfile: {file}\n\n')
+                    # print(f"src path: {source_path}, \nrel path: {relative_path}, \nDest Path: {dest_path}\n\n")
                     try:
                         cp_cmd = f'sudo cp "{source_path}" "{dest_path}"'
                         run_command(cp_cmd)
@@ -50,8 +49,75 @@ def mount_and_extract_text_files(image_path, output_dir, start_sector):
         run_command(f'sudo umount "{ewf_mount_point}"')
 
 
+
+def search_keywords_in_txt_files(txt_files, keywords):
+    """Searches for keywords in extracted .txt files"""
+    keyword_found = 0
+    keywords = [kw.strip().lower() for kw in keywords]
+    results = {kw:['nil'] for kw in keywords}
+
+    for file_path in txt_files:
+        try:
+            with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+                content = f.read()
+
+            sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s', content)
+            
+            for keyword in keywords:
+                res = []
+                flag=0
+                for sentence in sentences:
+                    if keyword in sentence.lower():
+                        res.append((file_path, sentence.strip()))
+                        keyword_found += 1
+                        flag=1
+                if flag == 1:
+                    results[keyword] = res
+
+        except Exception as e:
+            print(f"Error reading {file_path}: {e}")
+
+
+    if keyword_found > 0:
+        for kw, found_list in results.items():
+            print(f"\n{"--"*60}\nKeyword: {kw} -->")
+
+            if isinstance(found_list, str) or (len(found_list) == 1 and isinstance(found_list[0], str)):
+                print(f"{found_list[0]}\n")
+                continue
+
+            else:
+                for file, sent in found_list:
+                    print(f"- {file}: {sent}\n")
+        return results
+
+    else:
+        print(f"Keyword(s) Not Present.")
+        return "Keyword(s) Not Present."
+
+
+def get_txt_file_paths(folder_path):
+    txt_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(".txt"):
+                txt_files.append(os.path.join(root, file))
+    return txt_files
+
+
+
+def MasterFunc(image_path, keywords, output_dir, start_sector):
+    mount_and_extract_text_files(image_path, output_dir, start_sector)
+    txt_paths = get_txt_file_paths(output_dir)
+    
+    txt_res = search_keywords_in_txt_files(txt_paths, keywords)
+    print(txt_res)
+    return txt_res
+
+
+
 # if __name__ == "__main__":
-#     image_path='TEST.E01'
+#     image_path='bootcamp.E01'
 #     output_dir='./output_files/extracted_files'
 
 #     image_name = os.path.basename(image_path)
@@ -59,5 +125,6 @@ def mount_and_extract_text_files(image_path, output_dir, start_sector):
 #     output_dir = os.path.join(output_dir, image_stem)
 
 #     start_sector = analyze_disk_image(image_path)
-
-#     mount_and_extract_text_files(image_path, output_dir, start_sector)
+#     keywords = ['inDUStry', 'Chomu', 'Infancy']
+#     # keywords = input("Enter Keywords (comma-separated, e.g.: Lorem, Ipsum, dolor): ").split(",")
+#     MasterFunc(image_path, keywords, output_dir, start_sector)
